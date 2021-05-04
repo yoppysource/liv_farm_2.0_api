22424e2c6f4640500438d97454bf8f5614ce286e
Yoppy <yoppylands@gmail.com>
2021-05-04 15:31:57 +0900

    add minor detail

--- utils/aligoApi.js
+++ utils/aligoApi.js
@@ -58,8 +58,8 @@ exports.sendAlimtalk = async (req, res) => {
     recvname: "리브팜",
     receiver_2: process.env.ADMIN_PHONE_NUMBER,
     subject_2: "주문알림",
-    message_2: `[LivFarm] 주문접수안내
-    안녕하세요, ${req.data.buyer_name}님으로부터 주문이 들어왔습니다.
+    message_2: `[LivFarm] 주문완료안내
+    안녕하세요, ${req.data.buyer_name}님. 리브팜에서 주문해주셔서 감사합니다. 배송 예정시간에 맞게 갓 수확한 채소를 신선하게 보내드리겠습니다.
     □ 주문명 : ${req.data.name}
     □ 배송지 : ${req.data.buyer_addr}
     □ 배송예정일 : ${req.data.customData.scheduledDate}
